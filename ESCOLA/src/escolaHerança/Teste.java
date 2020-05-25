07a4a56fc7d5d193ece43ee2f6729eb1f94f4bfc
Dalyla <Dalyla@Dalyla-PC>
2020-05-25 17:43:43 -0300

    INICIO PROJETO ESCOLA (TESTE HERANÇA)

---
+++ ESCOLA/src/escolaHerança/Teste.java
@@ -0,0 +1,25 @@
+package escolaHerança;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+		
+		aluno.setNome("Maria");
+		aluno.setEndereco("Rua 3");
+		aluno.setCpf("99999999999");
+		aluno.setCurso("Sistemas");
+		aluno.setTelefone("88977777777");
+		
+		professor.setNome("Pedro");
+		professor.setEndereco("Rua 10");
+		professor.setCpf("66666666666");
+		professor.setNomeCurso("Sstemas");
+		professor.setTelefone("85988888888");
+		
+		
+	}
+
+}
